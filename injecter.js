f8549dc2b5756212adb4a9de38d34e84902a2777
Rick Rongen <rickrongen@outlook.com>
2016-12-27 21:04:27 +0100

    I think I just added support for firefox, not sure

--- injecter.js
+++ injecter.js
@@ -1,5 +1,11 @@
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('sausage.js');
+if(window.chrome){
+    s.src = chrome.extension.getURL('sausage.js');
+}else if(window.browser){
+    s.src = browser.extension.getURL("sausage.js");
+}else{
+   alert('What kind of browser is this? I don\'t know how to use it!');
+}
 
 //todo unload?
 (document.head || document.documentElement).appendChild(s);
