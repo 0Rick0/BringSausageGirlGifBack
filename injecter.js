dc95b664e68e548a96e9ba3285432a6f770d299e
Rick Rongen <rickrongen@outlook.com>
2016-12-30 17:37:13 +0100

    Fixed firefox support, now only need to figure out how to pack it...

--- injecter.js
+++ injecter.js
@@ -1,7 +1,7 @@
 var s = document.createElement('script');
 if(window.chrome){
     s.src = chrome.extension.getURL('sausage.js');
-}else if(window.browser){
+}else if(browser){
     s.src = browser.extension.getURL("sausage.js");
 }else{
    alert('What kind of browser is this? I don\'t know how to use it!');
